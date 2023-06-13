3663f0b18c6a513b4945de7d390a4ea1c73fd2b6
Omer Oguz Celikel <44753206+OmerCelikel@users.noreply.github.com>
2023-06-13 18:32:13 +0300

    view hide

--- sensorApp/sensorApp/Network/AFService.swift
+++ sensorApp/sensorApp/Network/AFService.swift
@@ -41,8 +41,8 @@ struct Gas: Codable {
 
 class AFGasService {
     //phone
-//    private let baseURL = "http://192.168.43.56:5000"
-    private let baseURL = "http://192.168.1.42:5000"
+    private let baseURL = "http://192.168.43.56:5000"
+//    private let baseURL = "http://192.168.1.42:5000"
     //base
 //    private let baseURL = "http://127.0.0.1:5000"
     

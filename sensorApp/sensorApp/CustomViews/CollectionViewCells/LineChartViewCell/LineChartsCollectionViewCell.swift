0480da7364f98c74413f60a93ac85c83fdca1a52
Omer Oguz Celikel <44753206+OmerCelikel@users.noreply.github.com>
2023-06-15 13:47:23 +0300

    Gas Values with colors

--- sensorApp/sensorApp/CustomViews/CollectionViewCells/LineChartViewCell/LineChartsCollectionViewCell.swift
+++ sensorApp/sensorApp/CustomViews/CollectionViewCells/LineChartViewCell/LineChartsCollectionViewCell.swift
@@ -16,12 +16,25 @@ class LineChartsCollectionViewCell: UICollectionViewCell {
         
     @IBOutlet weak var graphView: GraphView!
     
+    private var previousGasValue: Double = 1000.0
+    private let percentageIncrease: Double = 0.15  // 5% increase
+
     func setup(with gasData: Gas) {
+        // Store the previous value before updating it
+        let currentGasValue = gasData.value
         
+        // Update the UI with the current gas value
         gasNameTextLabel.text = gasData.name
-        gasValueTextLabel.text = String(format: "%.2f",gasData.value)
-        self.gasValueTextLabel.textColor = (gasData.value > self.gasValueTextLabel.text.flatMap(Double.init) ?? 0) ? .green : .red
-//        self.graphView.backgroundColor = (gasData.value > (self.gasValueTextLabel.text.flatMap(Double.init) ?? 0.0) * 0.15 ) ? .white : .red
+        gasValueTextLabel.text = String(format: "%.2f", currentGasValue)
+        
+        // Compare the current value with the previous value and set the text color
+        let thresholdValue = previousGasValue + (previousGasValue * percentageIncrease)
+        self.gasValueTextLabel.textColor = (currentGasValue > thresholdValue) ? .red : .systemGreen
+        
+        // Update the previousGasValue for the next setup call
+        previousGasValue = currentGasValue
+        
+        // Rest of the setup code
     }
     
     func setupEmpty() {
